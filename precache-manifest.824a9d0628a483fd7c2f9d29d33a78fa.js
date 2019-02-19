8a9f3a0139e223f6c1c01aa7c708c3431264b072
Eugen Steffen <stalepresh121@outlook.com>
2019-02-19 18:22:12 +0200

    Updates

---
+++ precache-manifest.824a9d0628a483fd7c2f9d29d33a78fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "97614287fe9dea9a8cb5",
+    "url": "/static/js/main.97614287.chunk.js"
+  },
+  {
+    "revision": "159f329f1ed1b3708ed0",
+    "url": "/static/js/1.159f329f.chunk.js"
+  },
+  {
+    "revision": "97614287fe9dea9a8cb5",
+    "url": "/static/css/main.0f3fc4a3.chunk.css"
+  },
+  {
+    "revision": "64d1555c9bd39b7cc892bcb61fbace2c",
+    "url": "/index.html"
+  }
+];
