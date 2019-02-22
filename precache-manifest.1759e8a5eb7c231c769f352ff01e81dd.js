590e8405a9d583ff905c7fa8384ba8097a6b025b
Eugen Steffen <stalepresh121@outlook.com>
2019-02-22 12:20:55 +0200

    Updates

--- precache-manifest.1759e8a5eb7c231c769f352ff01e81dd.js
+++ precache-manifest.1759e8a5eb7c231c769f352ff01e81dd.js
@@ -1,31 +1,31 @@
 self.__precacheManifest = [
   {
-    "revision": "f4769f9bdb7466be65088239c12046d1",
-    "url": "./static/media/glyphicons-halflings-regular.f4769f9b.eot"
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "./static/media/glyphicons-halflings-regular.448c34a5.woff2"
   },
   {
-    "revision": "85c2cc5e257aeec13cb3",
-    "url": "./static/css/main.f767d0fb.chunk.css"
+    "revision": "587f6ef7fa1f73037f8e",
+    "url": "./static/css/main.eb8b36c5.chunk.css"
   },
   {
     "revision": "89889688147bd7575d6327160d64e760",
     "url": "./static/media/glyphicons-halflings-regular.89889688.svg"
   },
   {
-    "revision": "e4324b2e143452650254",
-    "url": "./static/js/1.e4324b2e.chunk.js"
+    "revision": "07fe010c7f058ab4d034",
+    "url": "./static/js/1.07fe010c.chunk.js"
   },
   {
     "revision": "4a686d48d5a089750c49",
     "url": "./static/js/runtime~main.4a686d48.js"
   },
   {
-    "revision": "85c2cc5e257aeec13cb3",
-    "url": "./static/js/main.85c2cc5e.chunk.js"
+    "revision": "587f6ef7fa1f73037f8e",
+    "url": "./static/js/main.587f6ef7.chunk.js"
   },
   {
-    "revision": "448c34a56d699c29117adc64c43affeb",
-    "url": "./static/media/glyphicons-halflings-regular.448c34a5.woff2"
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "./static/media/glyphicons-halflings-regular.f4769f9b.eot"
   },
   {
     "revision": "fa2772327f55d8198301fdb8bcfc8158",
@@ -36,11 +36,11 @@ self.__precacheManifest = [
     "url": "./static/media/glyphicons-halflings-regular.e18bbf61.ttf"
   },
   {
-    "revision": "e4324b2e143452650254",
+    "revision": "07fe010c7f058ab4d034",
     "url": "./static/css/1.d4cdc168.chunk.css"
   },
   {
-    "revision": "fd5ddfc7c7b06914f1525a8f948170c6",
+    "revision": "b599d6823613056973a3eec0812582e6",
     "url": "./index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.b1453b062e91eab8a4da331874072234.js"
+  "./precache-manifest.1759e8a5eb7c231c769f352ff01e81dd.js"
 );
 
 workbox.clientsClaim();
