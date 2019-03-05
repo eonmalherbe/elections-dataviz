34a66ee3fed711fdd0d9ad3e6c2c0793a4328046
Eugen Steffen <stalepresh121@outlook.com>
2019-03-05 19:21:04 +0200

    Updates

---
+++ precache-manifest.2054a301faef938c5deb1f956ed63bf1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "./static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "4ff8f111767119e70113",
+    "url": "./static/css/main.3ee6f64c.chunk.css"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "./static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "d3265062aa4410eded18",
+    "url": "./static/js/1.d3265062.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "4ff8f111767119e70113",
+    "url": "./static/js/main.4ff8f111.chunk.js"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "./static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "./static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "./static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "d3265062aa4410eded18",
+    "url": "./static/css/1.268ffdc0.chunk.css"
+  },
+  {
+    "revision": "9ef7c70c3a5d97f46fa7da0f0ba587b0",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.12e711958f816731f1e2e1752c7b2531.js"
+  "./precache-manifest.2054a301faef938c5deb1f956ed63bf1.js"
 );
 
 workbox.clientsClaim();
