826d5160ec98dc0ecb0d37e29568b5a5bbb2957f
Adi Eyal <adi@openup.org.za>
2019-08-26 12:46:46 +0200

    Updates

--- precache-manifest.3c9d7c6d67c4526e9403df32c6dd9c7a.js
+++ precache-manifest.3c9d7c6d67c4526e9403df32c6dd9c7a.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "1769a9cf58b97d03ab44",
+    "revision": "50dbcc582ba5250e53da",
     "url": "./static/css/main.28707672.chunk.css"
   },
   {
-    "revision": "1769a9cf58b97d03ab44",
-    "url": "./static/js/main.1769a9cf.chunk.js"
+    "revision": "50dbcc582ba5250e53da",
+    "url": "./static/js/main.50dbcc58.chunk.js"
   },
   {
     "revision": "5fb5156c8e2abe2fe8fc",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "./static/js/runtime~main.4a686d48.js"
   },
   {
-    "revision": "4ea244fa7fedd215c1802c155f85eafd",
+    "revision": "d61c72c1aed80b0f990bfe38c9a81a9e",
     "url": "./index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.748f267638e81084d0d334347432ca08.js"
+  "./precache-manifest.3c9d7c6d67c4526e9403df32c6dd9c7a.js"
 );
 
 workbox.clientsClaim();
