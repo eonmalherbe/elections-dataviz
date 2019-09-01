b1cfa036bb749b69b9dfd74e11794d45f1e7aad8
Adi Eyal <adi@openup.org.za>
2019-09-01 21:04:53 +0200

    Updated docz

--- .docz/app/imports.js
+++ .docz/app/imports.js
@@ -1,14 +1,26 @@
 export const imports = {
   'src/index.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "src-index" */ 'src/index.mdx'),
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "src-index" */ 'src/index.mdx'
+    ),
   'src/components/quickresults.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "src-components-quickresults" */ 'src/components/quickresults.mdx'),
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "src-components-quickresults" */ 'src/components/quickresults.mdx'
+    ),
   'src/components/race-for-seats.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "src-components-race-for-seats" */ 'src/components/race-for-seats.mdx'),
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "src-components-race-for-seats" */ 'src/components/race-for-seats.mdx'
+    ),
   'src/components/race-for-votes.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "src-components-race-for-votes" */ 'src/components/race-for-votes.mdx'),
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "src-components-race-for-votes" */ 'src/components/race-for-votes.mdx'
+    ),
   'src/components/spoilt.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "src-components-spoilt" */ 'src/components/spoilt.mdx'),
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "src-components-spoilt" */ 'src/components/spoilt.mdx'
+    ),
   'src/components/turnout.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "src-components-turnout" */ 'src/components/turnout.mdx'),
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "src-components-turnout" */ 'src/components/turnout.mdx'
+    ),
 }
